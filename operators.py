32aa1db9847ae34873b7eb6d36e32d29eb0d988e
Alethia Quintero <alethiaq22@gmail.com>
2020-01-27 23:17:07 -0500

    Basic operators

---
+++ operators.py
@@ -0,0 +1,27 @@
+# basic addition, subtraction, multiplication
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# modulo is used to deduce a remainer
+remainder = 11 % 3
+print(remainder)
+
+# power symbol is made with two ** 
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+# strings can be concatenated with operators
+boopgoopdoop = "boop" + " " + "goop" + " " + "doop"
+print(boopgoopdoop)
+
+# you can multiply strings and lists with the operator and an interger to get a number of repeated strings
+thisIsCoolTenTimes = "this is cool!" * 10
+print(thisIsCoolTenTimes)
+
+# lists can be joined simply by an additon operator
+blue_food = ["blueberries"]
+green_foods = ["grape", "apple"]
+all_foods = blue_food + green_foods
+print(all_foods)
