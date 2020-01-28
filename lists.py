6009b6895666b53ad9357d9dd69132102b65b1f6
Alethia Quintero <alethiaq22@gmail.com>
2020-01-27 23:07:26 -0500

    Lists

---
+++ lists.py
@@ -0,0 +1,10 @@
+# similar to arrays, can be iterated over, limitless in size, zero-based, can contain any type of variable 
+list = []
+
+# can add to a list with .append method
+list.append("cat")
+list.append("bird")
+
+# can access items by index within bracket notation
+first = list[0]
+
